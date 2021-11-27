f7b9dc7438a691bf49f68f5efa6d18495d43e763
Marthe-M <martheminderman@gmail.com>
2021-11-27 16:33:01 +0100

    style changes

--- src/components/Bookcontainer.js
+++ src/components/Bookcontainer.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchBooks } from '../store/actions.js'
-import { StarTwoTone, BookOutlined } from '@ant-design/icons'
+import { StarTwoTone } from '@ant-design/icons'
 import { addNewFavorite } from '../store/actions.js'
 
 export default function Bookcontainer () {
@@ -10,38 +10,42 @@ export default function Bookcontainer () {
   const showResults = useSelector(state => state.showResults)
   const favoriteBooks = useSelector(state => state.favorites)
   const showFavorites = useSelector(state => state.showFavorites)
-  const listItems = bookData
-    .map(book => (
-      <li key={book.id} className='book-item'>
-        <BookOutlined style={{ fontSize: '30px' }} />
-
-        <h4>{book.volumeInfo.title}</h4>
-        <p>{book.volumeInfo.subtitle}</p>
-        <div className='line'></div>
-        <img
-          src={
-            book.volumeInfo.imageLinks === undefined
-              ? ''
-              : `${book.volumeInfo.imageLinks.thumbnail}`
-          }
-          alt='No book cover available'
-        />
-        <div className='favorite-box'>
-          <h3 className='favorite-text'>Favorite: </h3>
-          <button
-            type='submit'
-            className='favorite-button'
-            onClick={() => dispatch(addNewFavorite(book))}
-          >
-            <StarTwoTone twoToneColor='#ebc92f' style={{ fontSize: '23px' }} />
-          </button>
-        </div>
-      </li>
-    ))
-    .slice(0, showResults)
+  const listItems =
+    bookData &&
+    bookData
+      .map(book => (
+        <li key={book.id} className='book-item'>
+          <h4>{book.volumeInfo.title}</h4>
+          <p>{book.volumeInfo.subtitle}</p>
+          <div className='line'></div>
+          <img
+            src={
+              book.volumeInfo.imageLinks === undefined
+                ? ''
+                : `${book.volumeInfo.imageLinks.thumbnail}`
+            }
+            alt='No book cover available'
+          />
+          <div className='favorite-box'>
+            <h4 className='favorite-text'>Favorite: </h4>
+            <button
+              type='submit'
+              className='favorite-button'
+              onClick={() => dispatch(addNewFavorite(book))}
+            >
+              <StarTwoTone
+                twoToneColor='#ebc92f'
+                style={{ fontSize: '23px' }}
+              />
+            </button>
+          </div>
+        </li>
+      ))
+      .slice(0, showResults)
   const listFavorites = favoriteBooks.map(book => (
     <li key={book.id} className='book-item'>
       <h3>{book.volumeInfo.title}</h3>
+      <p>{book.volumeInfo.subtitle}</p>
 
       <img
         src={
